b00890a3bbd51d6d24afce43c7a4512878ebfb4d
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2024-03-22 21:23:46 -0700

    home page and navbar page

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import DataTable from '../../components/dataTables/DataTable'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+
+import "./list.scss"
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className="list">
+    <Sidebar/>
+    <div className="listContainer">
+      <Navbar/>
+
+      <DataTable 
+      // columns={columns}
+      />
+    </div>
+  </div>
   )
 }
 
