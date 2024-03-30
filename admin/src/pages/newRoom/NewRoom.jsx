1c345e7e5699d8e30dd062d625dbaa2e3185df37
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2024-03-29 18:51:03 -0700

    room creation

--- admin/src/pages/newRoom/NewRoom.jsx
+++ admin/src/pages/newRoom/NewRoom.jsx
@@ -1,56 +1,80 @@
 import React, { useState } from 'react'
-import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import axios from "axios"
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import { roomInputs } from "../../formSource";
 
 import "./newRoom.scss"
-const NewRoom = ({inputs,title}) => {
-  const [file, setFile] = useState("");
+import useFetch from '../../components/hooks/useFetch';
+const NewRoom = () => {
+  const [info, setInfo] = useState({});
+  const [hotelId, setHotelId] = useState("");
+  const [rooms, setRooms] = useState([]);
+
+  const { data, loading } = useFetch("/hotels");
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const roomNumbers = rooms.split(",").map((room) => ({ number: room }));
+    try {
+      await axios.post(`/rooms/${hotelId}`, { ...info, roomNumbers });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="new">
-      <Sidebar />
-      <div className="newContainer">
-        <Navbar />
-        <div className="top">
-          <h1>{title}</h1>
-        </div>
-        <div className="bottom">
-          <div className="left">
-            <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
-              alt=""
-            />
-          </div>
-          <div className="right">
-            <form>
-              <div className="formInput">
-                <label htmlFor="file">
-                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
-                </label>
+    <Sidebar />
+    <div className="newContainer">
+      <Navbar />
+      <div className="top">
+        <h1>Add New Room</h1>
+      </div>
+      <div className="bottom">
+        <div className="right">
+          <form>
+            {roomInputs.map((input) => (
+              <div className="formInput" key={input.id}>
+                <label>{input.label}</label>
                 <input
-                  type="file"
-                  id="file"
-                  onChange={(e) => setFile(e.target.files[0])}
-                  style={{ display: "none" }}
+                  id={input.id}
+                  type={input.type}
+                  placeholder={input.placeholder}
+                  onChange={handleChange}
                 />
               </div>
-              {inputs.map((input) => (
-                <div className="formInput" key={input.id}>
-                  <label>{input.label} </label>
-                  <input type={input.type} placeholder={input.placeholder} />
-                </div>
-              ))}
-
-              <button className="button">send</button>
-            </form>
-          </div>
+            ))}
+            <div className="formInput">
+              <label>Rooms</label>
+              <textarea
+                onChange={(e) => setRooms(e.target.value)}
+                placeholder="give comma between room numbers."
+              />
+            </div>
+            <div className="formInput">
+              <label>Choose a hotel</label>
+              <select
+                id="hotelId"
+                onChange={(e) => setHotelId(e.target.value)}
+              >
+                {loading
+                  ? "loading"
+                  : data &&
+                    data.map((hotel) => (
+                      <option key={hotel._id} value={hotel._id}>{hotel.name}</option>
+                    ))}
+              </select>
+            </div>
+            <button onClick={handleSubmit}>Send</button>
+          </form>
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
